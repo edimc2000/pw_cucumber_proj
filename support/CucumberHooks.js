8fea8799f9fefef879334179db3846352a140b8e
Eddie C. <edimc2000@gmail.com>
2025-06-18 16:22:29 -0500

    auto exit after 5 sec using hooks

--- support/CucumberHooks.js
+++ support/CucumberHooks.js
@@ -6,27 +6,35 @@ import { DynamicTablesPage } from "../tests/a-pages/DynamicTables.page.js";
 // This file runs before/after anything else in the steps folder
 setDefaultTimeout(60000);
 
-BeforeAll(async function() {
+BeforeAll(async function () {
   global.browser = await chromium.launch({
-    headless: process.env.CI ? true : false, 
+    headless: process.env.CI ? true : false,
   });
 });
 
-Before(async function() {
+Before(async function () {
   this.browser = global.browser;
   this.context = await this.browser.newContext();
   this.page = await this.context.newPage();
 
   //page objects 
-  this.dynamicTablesPage= new DynamicTablesPage (this.page)
+  this.dynamicTablesPage = new DynamicTablesPage(this.page)
 });
 
-After(async function() {
+After(async function () {
   await this.page.close();
   await this.context.close();
 });
 
-AfterAll(async function() {
+AfterAll(async function () {
   await this.browser?.close();
   // process.exit()
+
+
+  setTimeout(() => {
+    console.log("Timeout reached, exiting process.");
+    process.exit(1); // Exit with a non-zero code to indicate an error/timeout
+  }, 5000); // Exit after 5000 milliseconds (5 seconds)
+
+  console.log("Process started, waiting for timeout...");
 });
