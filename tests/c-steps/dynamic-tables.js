df31706a4dc442bba65bd4955ef79f7570874f76
Eddie C. <edimc2000@gmail.com>
2025-06-19 11:48:13 -0500

    2nd runner

--- tests/c-steps/dynamic-tables.js
+++ tests/c-steps/dynamic-tables.js
@@ -72,7 +72,6 @@ Then(`the user enters the price as “{int}”`, async function(price) {
     await this.dynamicTablesPage.getFieldPrice.fill(`${price}`) 
 });
 
-
 Then(`the user should see the table with the new row below`, async function(DataTable) {
     const expectedValues = DataTable.rawTable[0]
     const actualValues =  await this.dynamicTablesPage.getRowLast.allInnerTexts()
